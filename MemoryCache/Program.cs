97f9ad3864f7d733ff4b92817a1c820b0a16a5c1
SohrabGheisari <codecell.ir@gmail.com>
2023-04-11 21:19:13 +0430

    fix service registration

--- MemoryCache/Program.cs
+++ MemoryCache/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddMemoryCache();
 builder.Services.AddScoped(typeof(ICacheManager),typeof(CacheManager));
 
 var app = builder.Build();
